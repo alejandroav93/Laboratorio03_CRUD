e24437f5bdbe249b6b1677571edc4fd04a903e1b
Alejandro Alvarez V <59927580+alejandroav93@users.noreply.github.com>
2021-02-25 11:24:38 -0600

    Added News Layout + Constructor Methods News Adapter

---
+++ app/src/main/java/com/example/crud_newslist/NewsAdapter.kt
@@ -0,0 +1,23 @@
+package com.example.crud_newslist
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class NewsAdapter: RecyclerView.Adapter<NewsAdapter.NewsHolder>() {
+
+    class NewsHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NewsHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: NewsHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+}
