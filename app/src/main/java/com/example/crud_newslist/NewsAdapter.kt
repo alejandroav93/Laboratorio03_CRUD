021d5493cac1620b309a6cd9bc4a952948f5eba9
Alejandro Alvarez V <59927580+alejandroav93@users.noreply.github.com>
2021-02-25 14:46:24 -0600

    Updated News Addapter with viewholder

--- app/src/main/java/com/example/crud_newslist/NewsAdapter.kt
+++ app/src/main/java/com/example/crud_newslist/NewsAdapter.kt
@@ -1,23 +1,32 @@
 package com.example.crud_newslist
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class NewsAdapter: RecyclerView.Adapter<NewsAdapter.NewsHolder>() {
+class NewsAdapter(private var noticias: MutableList<String>): RecyclerView.Adapter<NewsAdapter.NewsHolder>() {
 
-    class NewsHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+    class NewsHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+        fun bind(noticia: String) = with(itemView){
+            val txtTitle: TextView = findViewById(R.id.txtTitle)
+            txtTitle.text = noticia
+        }
+    }
+
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NewsHolder {
-        TODO("Not yet implemented")
+        val view: View = LayoutInflater.from(parent.context).inflate(R.layout.item_news, parent, false)
+        return NewsHolder(view)
     }
 
     override fun onBindViewHolder(holder: NewsHolder, position: Int) {
-        TODO("Not yet implemented")
+        val actual = this.noticias[position]
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+       return this.noticias.size
     }
 
 }
